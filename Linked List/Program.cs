3ac992e1ca27babc9aca0951d46ea0892caaff38
Bhavana <bhavananerlakota@gmail.com>
2022-04-28 01:38:48 +0530

    [Bhavana]:Added UC4 problem

--- Linked List/Program.cs
+++ Linked List/Program.cs
@@ -8,11 +8,11 @@ namespace Linked_List
     {
         static void Main(string[] args)
         {
-            CustomLinkedList<int> list = new CustomLinkedList<int>(); 
-            list.AddFirst(70); 
-            list.AddFirst(30); 
-            list.AddFirst(56); 
-            list.DisplayElement();
+            CustomLinkedList<int> list = new CustomLinkedList<int>(); // creating a object of in built LinkedList generic class           
+            list.Append(56); // Calling method of CustomLinkedList generic class to append 56
+            list.Append(70); // Calling method of CustomLinkedList generic class to append 70
+            list.insertBetween(new Node<int>(56), 30, new Node<int>(70)); // Calling method of CustomLinkedList generic class to append 30 
+            list.DisplayElement(); // Calling method of CustomLinkedList generic class to display linkedlist element
             Console.ReadLine();
         }
     }
