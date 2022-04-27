195b0f9cac6f75706eb9a1b9ac52fc16648f4e9f
Bhavana <bhavananerlakota@gmail.com>
2022-04-28 00:39:53 +0530

    [Bhavana]:Added welcome message

---
+++ Linked List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 14 Linked list problem");
+        }
+    }
+}
